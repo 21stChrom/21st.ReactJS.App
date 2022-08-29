90d509cbe7f324bd01553fa292fafe259fcd9d1e
21stChrom <parkerjmartin2020@gmail.com>
2022-08-28 22:45:14 -0400

    3 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import StopWatch from './Components/StopWatch/StopWatch.jsx';
 function App() {
   return (
     <div className="App">
-      <StopWatch />
-      <div>
+        <div>
       <Webimage />
       </div>
+      <StopWatch />
       <iframe
         id="embed"
         src="https://react-exdcin.stackblitz.io?embed=1&file=src/style.css?embed=1&file=index.js, src/App.js"
